29872b6e7eaee6d8e446691fe6823f3078727d8e
Raysen <raysen@live.cn>
2021-02-13 22:43:23 +0800

    Update pathes for custom

--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -10,14 +10,14 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/exynos/private
+    device/custom/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/system
+    device/custom/sepolicy/exynos/dynamic \
+    device/custom/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/vendor
+    device/custom/sepolicy/exynos/dynamic \
+    device/custom/sepolicy/exynos/vendor
 endif
